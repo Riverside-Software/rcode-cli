fead5369e6e8f53006258ae62e1ed95360fe4231
Gilles Querret <g.querret@riverside-software.fr>
2023-02-02 15:49:05 +0100

    Scan & Compare command line

---
+++ README.md
@@ -0,0 +1,32 @@
+## Description
+
+Command line utility to display rcode information, and to compare two directories with rcode.
+
+## Requirements
+
+Java 11
+
+## Scan command line
+
+```
+java -jar rcode-cli.jar scan /path/to/dir
+```
+
+Sample output:
+```
+   CRC                                       Digest File name
+ 13701 hSHCHhDPhk/3fWfeeeRewGbZi5kXHM+y7tbuqe+p/Y4= pct\compile.r
+ 36002 UR44rlrNd1+yyGg8skstvoQf6Hsi5vJ9gJuI7zhAPlc= pct\dmpSch.r
+ 56703 Xr9IjGTe6Zx57nhRGo2NVSCgkQNC0qioKsvmsC3Zvz8= pct\dump_seq.r
+ 36350 u08h/C8V9CeZNfu3wii2aYrgnOdv7kVGJJ63hOaKBf8= pct\dynrun.r
+ ...
+```
+
+## Compare command line
+
+```
+java -jar rcode-cli.jar scan /path/to/dir1 /path/to/dir2
+```
+
+Output only shows file present in first directory and not in the second directory, as well as rcodes that have a different CRC or digest.
+Output has one file per line, so it's easy to pipe the input to another command.
--- src/main/java/eu/rssw/openedge/RCodeInfoCli.java
+++ src/main/java/eu/rssw/openedge/RCodeInfoCli.java
@@ -19,23 +19,148 @@
  */
 package eu.rssw.openedge;
 
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.PrintStream;
+import java.nio.file.FileVisitResult;
+import java.nio.file.FileVisitor;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
+import com.beust.jcommander.Parameters;
 
 import eu.rssw.pct.RCodeInfo;
 import eu.rssw.pct.RCodeInfo.InvalidRCodeException;
 
 public class RCodeInfoCli {
+  private static CompareCommand compare = new CompareCommand();
+  private static ScanCommand scan = new ScanCommand();
+
+  private PrintStream out;
+  private PrintStream err;
+
+  public RCodeInfoCli() {
+    this(System.out, System.err);
+  }
+
+  public RCodeInfoCli(PrintStream out, PrintStream err) {
+    this.out = out;
+    this.err = err;
+  }
 
   public static void main(String[] args) throws IOException, InvalidRCodeException {
-    try (InputStream input = new FileInputStream(args[0])) {
-      RCodeInfo rci = new RCodeInfo(input);
-      System.out.println("Version: " + (rci.getVersion() & 0x3FFF) + (rci.is64bits() ? " 64 bits" : ""));
-      System.out.println("Timestamp: " + rci.getTimeStamp());
-      System.out.println("CRC: " + rci.getCrc());
-      if (rci.getDigest() != null)
-        System.out.println("Digest: " + rci.getDigest());
+    RCodeInfoCli main = new RCodeInfoCli();
+    JCommander jc = new JCommander(main);
+    jc.addCommand("compare", compare);
+    jc.addCommand("scan", scan);
+
+    try {
+      jc.parse(args);
+    } catch (ParameterException caught) {
+      jc.usage();
+      System.exit(1);
+    }
+    try {
+      if ("compare".equals(jc.getParsedCommand()))
+        main.executeCompare();
+      else if ("scan".equals(jc.getParsedCommand()))
+        main.executeScan();
+
+    } catch (IOException caught) {
+      main.out.println("I/O problem: " + caught.getMessage());
+      System.exit(1);
+    }
+
+  }
+
+  private void executeScan() throws IOException {
+    FileCollector fc1 = new FileCollector(scan.libs.get(0));
+    Files.walkFileTree(fc1.initPath, fc1);
+
+    out.printf("%6s %44s %s%n", "CRC", "Digest", "File name");
+    fc1.files.stream().forEach(it -> {
+      try (InputStream s1 = Files.newInputStream(fc1.initPath.resolve(it))) {
+        RCodeInfo rci1 = new RCodeInfo(s1);
+        out.printf("%6s %44s %s%n", rci1.getCrc(), rci1.getDigest(), it);
+      } catch (IOException | InvalidRCodeException caught) {
+        err.printf("Unable to read %s%n" + it);
+        caught.printStackTrace(err);
+      }
+    });
+  }
+
+  private void executeCompare() throws IOException {
+    FileCollector fc1 = new FileCollector(compare.libs.get(0));
+    FileCollector fc2 = new FileCollector(compare.libs.get(1));
+    Files.walkFileTree(fc1.initPath, fc1);
+    Files.walkFileTree(fc2.initPath, fc2);
+
+    fc1.files.stream().forEach(it -> {
+      if (!fc2.files.contains(it)) {
+        out.printf("%s%n", it);
+      } else {
+        try (InputStream s1 = Files.newInputStream(compare.libs.get(0).resolve(it));
+            InputStream s2 = Files.newInputStream(compare.libs.get(1).resolve(it))) {
+          RCodeInfo rci1 = new RCodeInfo(s1);
+          RCodeInfo rci2 = new RCodeInfo(s2);
+          if ((rci1.getCrc() != rci2.getCrc()) || !rci1.getDigest().equals(rci2.getDigest())) {
+            out.printf("%s%n", it);
+          }
+        } catch (IOException | InvalidRCodeException caught) {
+          err.printf("Unable to read %s%n" + it);
+          caught.printStackTrace(err);
+        }
+      }
+    });
+  }
+
+  private static class FileCollector implements FileVisitor<Path> {
+    private final List<String> files = new ArrayList<>();
+    private final Path initPath;
+
+    public FileCollector(Path initPath) {
+      this.initPath = initPath;
+    }
+
+    @Override
+    public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+      return FileVisitResult.CONTINUE;
+    }
+
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+      if (file.getFileName().toString().endsWith(".r"))
+        files.add(initPath.relativize(file).toString());
+      return FileVisitResult.CONTINUE;
+    }
+
+    @Override
+    public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
+      return FileVisitResult.CONTINUE;
+    }
+
+    @Override
+    public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+      return FileVisitResult.CONTINUE;
     }
   }
+
+  @Parameters(commandDescription = "Compare two rcode directories")
+  public static class CompareCommand {
+    @Parameter(arity = 2, description = "sourceDir targetDir", required = true)
+    private List<Path> libs;
+  }
+
+  @Parameters(commandDescription = "Scan rcode directory")
+  public static class ScanCommand {
+    @Parameter(arity = 1, description = "sourceDir", required = true)
+    private List<Path> libs;
+  }
+
 }
